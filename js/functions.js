b867fa2febf46e9c86b670304d968a46940bc002
Alexey <alexeylychagin@gmail.com>
2023-06-12 19:10:16 +0300

    правка

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,20 @@
-let checkLength = function (str, maxLength) {
+const checkLength = function (str, maxLength) {
   return str.length <= maxLength;
 };
 
 
-let checkPalindrome = function (str) {
+const checkPalindrome = function (str) {
   str = str.replace(/\s+/g, '').toLowerCase();
   let newStr = '';
-  for (let i = str.length-1; i >= 0; i--) {
+  for (let i = str.length - 1; i >= 0; i--) {
     newStr += str[i];
-  };
+  }
   return newStr === str;
 };
 
 
-let createNumber = function(str) {
-  let string = str.toString();
-  let number = string.match(/\d+/g).toString().replaceAll(',', '');
+const createNumber = function(str) {
+  const string = str.toString();
+  const number = string.match(/\d+/g).toString().replaceAll(',', '');
   return parseInt(number, 10);
 };
