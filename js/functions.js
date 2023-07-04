5731fe5b281d4f8cfa0c7e60d8f1fba694753f59
Alexey <alexeylychagin@gmail.com>
2023-07-04 23:03:39 +0300

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,26 @@ const createNumber = function(str) {
   }
 };
 createNumber('1 кефир, 0.5 батона');
+
+const meetingCheck = (workDayStart, workDayEnd, meetingStart, meetingDuration) => {
+  const timeDivision = (time) => {
+    const hoursMinutes = time.split(':');
+    return hoursMinutes;
+  };
+  const convertToMinutes = (hoursMinutes) => {
+    const minutes = parseInt(hoursMinutes[0], 10) * 60 + parseInt(hoursMinutes[1], 10);
+    return minutes;
+  };
+  const meetingEnd = convertToMinutes(timeDivision(meetingStart)) + meetingDuration;
+  if (convertToMinutes(timeDivision(workDayStart)) > convertToMinutes(timeDivision(meetingStart)) || convertToMinutes(timeDivision(workDayEnd)) < meetingEnd) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+meetingCheck('08:00', '17:30', '14:00', 90);
+meetingCheck('8:0', '10:0', '8:0', 120);
+meetingCheck('08:00', '14:30', '14:00', 90);
+meetingCheck('14:00', '17:30', '08:0', 90);
+meetingCheck('8:00', '17:30', '08:00', 900);
