f691ee20075c5b644f8e063587ec4f0e1cb7e1fa
Alexey <alexeylychagin@gmail.com>
2023-07-13 22:27:35 +0300

    Отрисуй меня полностью

---
+++ js/thumbnails.js
@@ -0,0 +1,25 @@
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+
+const createThumbnail = (post) => {
+  const { comments, description, likes, url } = post;
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+
+  return thumbnail;
+};
+
+const renderThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.appendChild(thumbnail);
+  });
+  container.appendChild(fragment);
+};
+
+export { renderThumbnails };
